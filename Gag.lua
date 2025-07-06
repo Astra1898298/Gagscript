083e8d5b7f01ce2e9e235b65f89c61b73b196c89
Growagardendupe <shinsabu925@gmail.com>
2025-07-06 09:53:13 +0900

    Gag.lua

--- Gag.lua
+++ Gag.lua
@@ -1,9 +1,10 @@
-If game.PlaceId == 126884695634066 then
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Growagarden X", HidePremium = false, SaveConfig = true, ConfigFolder = "TutorialConfig"})
+if game.PlaceId == 126884695634066 then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+local Window = OrionLib:MakeWindow({Name = "Growagarden X",HidePremium = false,SaveConfig = true,ConfigFolder = "TutorialConfig"})
 
 
 
 
-end
-OrionLib:init()
+    
+  end
+OrionLib:Init()
